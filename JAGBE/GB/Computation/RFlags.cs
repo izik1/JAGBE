94fe750d30238ca5fe0df219224bf317cb389760
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2017-08-05 13:46:51 -0700

    Add all flag-bit combos

--- JAGBE/GB/Computation/RFlags.cs
+++ JAGBE/GB/Computation/RFlags.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// A Bit-field of the Carry flag;
         /// </summary>
-        internal const byte CB = 1 << CF;
+        internal const byte CB = 0b0001_0000;
 
         /// <summary>
         /// Carry flag.
@@ -18,7 +18,9 @@
         /// <summary>
         /// A Bit-field of the Half Carry flag;
         /// </summary>
-        internal const byte HB = 1 << HF;
+        internal const byte HB = 0b0010_0000;
+
+        internal const byte HCB = 0b0011_0000;
 
         /// <summary>
         /// Half carry flag (BCD)
@@ -28,21 +30,39 @@
         /// <summary>
         /// A Bit-field of the Add/Sub flag;
         /// </summary>
-        internal const byte NB = 1 << NF;
+        internal const byte NB = 0b0100_0000;
+
+        internal const byte NCB = 0b0101_0000;
 
         /// <summary>
         /// Add/Sub flag (BCD)
         /// </summary>
         internal const byte NF = 6;
 
+        internal const byte NHB = 0b0110_0000;
+
+        internal const byte NHCB = 0b0111_0000;
+
         /// <summary>
         /// A Bit-field of the Zero flag;
         /// </summary>
-        internal const byte ZB = 1 << ZF;
+        internal const byte ZB = 0b1000_0000;
+
+        internal const byte ZCB = 0b1001_0000;
 
         /// <summary>
         /// Zero flag.
         /// </summary>
         internal const byte ZF = 7;
+
+        internal const byte ZHB = 0b1010_0000;
+        internal const byte ZHCB = 0b1011_0000;
+
+        internal const byte ZNB = 0b1100_0000;
+        internal const byte ZNCB = 0b1101_0000;
+
+        internal const byte ZNHB = 0b1110_0000;
+
+        internal const byte ZNHCB = 0b1111_0000;
     }
 }
