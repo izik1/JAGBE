40e23793784f56a5e0733ae533cf6ba1c0048349
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2017-08-09 15:33:31 -0700

    Add template for operations

--- JAGBE/GB/Computation/Execution/Alu/Bitwise.cs
+++ JAGBE/GB/Computation/Execution/Alu/Bitwise.cs
@@ -4,6 +4,8 @@ namespace JAGBE.GB.Computation.Execution.Alu
 {
     internal static class Bitwise
     {
+        private delegate byte Op(GbMemory mem, byte valIn, byte dest);
+
         public static bool Bit(Opcode code, GbMemory memory, int step)
         {
             if (step == 0)
@@ -359,6 +361,37 @@ namespace JAGBE.GB.Computation.Execution.Alu
                 case 2:
                     memory.SetMappedMemoryHl((byte)((code.Data1 << 4) | (code.Data1 >> 4)));
                     memory.R.F = code.Data1 == 0 ? RFlags.ZB : (byte)0;
+                    return true;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(step));
+            }
+        }
+
+        private static bool Operate(Opcode op, GbMemory memory, int step, Op operation)
+        {
+            if (operation == null)
+            {
+                throw new ArgumentNullException(nameof(operation));
+            }
+
+            switch (step)
+            {
+                case 0:
+                    if (op.Src == 6)
+                    {
+                        return false;
+                    }
+
+                    memory.R.SetR8(op.Src, operation(memory, memory.R.GetR8(op.Src), op.Dest));
+                    return true;
+
+                case 1:
+                    op.Data1 = memory.GetMappedMemoryHl();
+                    return false;
+
+                case 2:
+                    memory.SetMappedMemoryHl(operation(memory, op.Data1, op.Dest));
                     return true;
 
                 default:
