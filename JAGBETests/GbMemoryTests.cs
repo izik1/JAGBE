d913265c1f63c84084e2d20f4346176b8aa74361
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2017-08-15 20:36:39 -0700

    Add test for edge case

---
+++ JAGBETests/GbMemoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JAGBE.GB.Computation;
+
+namespace JAGBETests
+{
+    [TestClass]
+    public class GbMemoryTests
+    {
+        [TestMethod]
+        [TestCategory("Construction")]
+        public void MemoryConstructorsAllowNull()
+        {
+#pragma warning disable RECS0026 // Possible unassigned object created by 'new'
+            new GbMemory();
+            new GbMemory(null);
+#pragma warning restore RECS0026 // Possible unassigned object created by 'new'
+        }
+    }
+}
