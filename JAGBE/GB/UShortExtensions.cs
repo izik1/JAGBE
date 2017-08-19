042442b7d1fdbf864e0fd7cffa4a08102ebbce85
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2017-08-19 15:17:43 -0700

    Document UshortExtensions

--- JAGBE/GB/UShortExtensions.cs
+++ JAGBE/GB/UShortExtensions.cs
@@ -1,7 +1,16 @@
 ﻿namespace JAGBE.GB
 {
+    /// <summary>
+    /// Provides extension methods for <see cref="ushort"/>
+    /// </summary>
     internal static class UShortExtensions
     {
-        public static bool GetHalfCarry(this ushort a, ushort b) => (((a & 0xFFF) + (b & 0xFFF)) & 0x1000) == 0x1000;
+        /// <summary>
+        /// determines weather adding two ushorts would produce a half carry.
+        /// </summary>
+        /// <param name="u">The first value.</param>
+        /// <param name="val">The second value.</param>
+        /// <returns></returns>
+        public static bool GetHalfCarry(this ushort u, ushort val) => (((u & 0xFFF) + (val & 0xFFF)) & 0x1000) == 0x1000;
     }
 }
