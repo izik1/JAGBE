d724df25d2d5d891a3baa1a6850c1c95e2536dd9
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2017-08-19 15:00:32 -0700

    Document the GameBoy class

--- JAGBE/GB/GameBoy.cs
+++ JAGBE/GB/GameBoy.cs
@@ -3,13 +3,30 @@ using JAGBE.GB.Computation;
 
 namespace JAGBE.GB
 {
+    /// <summary>
+    /// A UI level Wrapper for accessing running a <see cref="Cpu"/>
+    /// </summary>
     internal sealed class GameBoy
     {
+        /// <summary>
+        /// The cpu
+        /// </summary>
         internal Cpu cpu;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameBoy"/> class.
+        /// </summary>
+        /// <param name="romPath">The rom path.</param>
+        /// <param name="BootromPath">The bootrom path.</param>
+        /// <param name="inputHandler">The input handler.</param>
         internal GameBoy(string romPath, string BootromPath, Input.IInputHandler inputHandler) =>
             this.cpu = new Cpu(File.ReadAllBytes(BootromPath), File.ReadAllBytes(romPath), inputHandler);
 
+        /// <summary>
+        /// Updates the cpu using the given <paramref name="targetUpdateRate"/> to determine the
+        /// number of ticks to run.
+        /// </summary>
+        /// <param name="targetUpdateRate">The target update rate.</param>
         internal void Update(int targetUpdateRate) => this.cpu.Tick(this.cpu.WriteToConsole ? 160 : Cpu.ClockSpeedHz / targetUpdateRate);
     }
 }
