d314b12c83a620ec76e57d39d3bf979acab9d45e
Zachery Gyurkovitz <zgyurkovitz@gmail.com>
2017-08-19 17:02:16 -0700

    Document Input (the entire namespace)

--- JAGBE/GB/Input/IInputHandler.cs
+++ JAGBE/GB/Input/IInputHandler.cs
@@ -2,8 +2,14 @@
 
 namespace JAGBE.GB.Input
 {
+    /// <summary>
+    /// An interface allows for generic registered input functions
+    /// </summary>
     internal interface IInputHandler
     {
+        /// <summary>
+        /// Occurs when Input is recieved.
+        /// </summary>
         event EventHandler<InputEventArgs> OnInput;
     }
 }
--- JAGBE/GB/Input/InputEventArgs.cs
+++ JAGBE/GB/Input/InputEventArgs.cs
@@ -2,10 +2,21 @@
 
 namespace JAGBE.GB.Input
 {
+    /// <summary>
+    /// Represents data for an event triggered by input.
+    /// </summary>
+    /// <seealso cref="EventArgs"/>
     internal sealed class InputEventArgs : EventArgs
     {
+        /// <summary>
+        /// The value of this instance
+        /// </summary>
         public readonly byte value;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputEventArgs"/> class.
+        /// </summary>
+        /// <param name="val">The value.</param>
         public InputEventArgs(byte val) => this.value = val;
     }
 }
